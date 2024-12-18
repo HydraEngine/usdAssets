987990a9da1f97625a0d79bbd1fa038c474da05a
yangfengzzz <yangfengzzz@hotmail.com>
2024-12-18 13:04:31 +0800

    add joint

--- scripts/joint.py
+++ scripts/joint.py
@@ -24,18 +24,11 @@ if __name__ == '__main__':
     boxActorPath = "/box0"
 
     # box0 props
-    position = Gf.Vec3f(0.0, 1000.0, 0.0)
-    orientation = Gf.Quatf(1.0)
-    color = Gf.Vec3f(165.0 / 255.0, 21.0 / 255.0, 21.0 / 255.0)
-    size = 100.0
-    scale = Gf.Vec3f(0.1, 0.1, 1)
-
     cubeGeom = UsdGeom.Cube.Define(stage, boxActorPath)
-    cubeGeom.CreateSizeAttr(size)
-    cubeGeom.AddTranslateOp().Set(position)
-    cubeGeom.AddOrientOp().Set(orientation)
-    cubeGeom.AddScaleOp().Set(scale)
-    cubeGeom.CreateDisplayColorAttr().Set([color])
+    cubeGeom.CreateSizeAttr(10)
+    cubeGeom.AddTranslateOp().Set(Gf.Vec3f(0.0, 0.0, 0.0))
+    cubeGeom.AddOrientOp().Set(Gf.Quatf(1.0))
+    cubeGeom.CreateDisplayColorAttr().Set([Gf.Vec3f(165.0 / 255.0, 21.0 / 255.0, 21.0 / 255.0)])
 
     # make it a static body - just apply PhysicsCollisionAPI
     cubePrim = stage.GetPrimAtPath(boxActorPath)
@@ -44,15 +37,12 @@ if __name__ == '__main__':
     # Box1 dynamic body
     boxActorPath = "/box1"
 
-    position = Gf.Vec3f(0.0, 1000.0, 120.0)
-    color = Gf.Vec3f(71.0 / 255.0, 165.0 / 255.0, 1.0)
-
     cubeGeom = UsdGeom.Cube.Define(stage, boxActorPath)
-    cubeGeom.CreateSizeAttr(size)
-    cubeGeom.AddTranslateOp().Set(position)
-    cubeGeom.AddOrientOp().Set(orientation)
-    cubeGeom.AddScaleOp().Set(scale)
-    cubeGeom.CreateDisplayColorAttr().Set([color])
+    cubeGeom.CreateSizeAttr(100)
+    cubeGeom.AddTranslateOp().Set(Gf.Vec3f(60.0, 0.0, 0.0))
+    cubeGeom.AddOrientOp().Set(Gf.Quatf(1))
+    cubeGeom.AddScaleOp().Set(Gf.Vec3f(0.1, 1, 0.1))
+    cubeGeom.CreateDisplayColorAttr().Set([Gf.Vec3f(71.0 / 255.0, 165.0 / 255.0, 1.0)])
 
     # setup dynamic rigid body
     cubePrim = stage.GetPrimAtPath(boxActorPath)
@@ -73,45 +63,47 @@ if __name__ == '__main__':
     revoluteJoint.CreateBody1Rel().SetTargets(["/box1"])
 
     # define revolute joint local poses for bodies
-    revoluteJoint.CreateLocalPos0Attr().Set(Gf.Vec3f(0.0, 60.0, 0.0))
-    revoluteJoint.CreateLocalRot0Attr().Set(Gf.Quatf(1.0))
+    orientation = Gf.Rotation([0, 1, 0], 90)
+    revoluteJoint.CreateLocalPos0Attr().Set(Gf.Vec3f(0.0, 0.0, 0.0))
+    revoluteJoint.CreateLocalRot0Attr().Set(Gf.Quatf(orientation.GetQuat()))
 
-    revoluteJoint.CreateLocalPos1Attr().Set(Gf.Vec3f(0.0, -60.0, 0.0))
-    revoluteJoint.CreateLocalRot1Attr().Set(Gf.Quatf(1.0))
+    revoluteJoint.CreateLocalPos1Attr().Set(Gf.Vec3f(-60.0, 0.0, 0.0))
+    revoluteJoint.CreateLocalRot1Attr().Set(Gf.Quatf(orientation.GetQuat()))
 
     # set break force/torque
     revoluteJoint.CreateBreakForceAttr().Set(1e20)
     revoluteJoint.CreateBreakTorqueAttr().Set(1e20)
 
     ##==================================================================================================================
-    # create revolute joint prim
-    distanceJoint = UsdPhysics.DistanceJoint.Define(stage, "/distanceJoint")
+    # # create revolute joint prim
+    # distanceJoint = UsdPhysics.DistanceJoint.Define(stage, "/distanceJoint")
+    #
+    # # define revolute joint axis and its limits, defined in degrees
+    # distanceJoint.CreateMinDistanceAttr(-10)
+    # distanceJoint.CreateMaxDistanceAttr(10.0)
+    #
+    # # define revolute joint bodies
+    # distanceJoint.CreateBody0Rel().SetTargets(["/box0"])
+    # distanceJoint.CreateBody1Rel().SetTargets(["/box1"])
+    #
+    # # define revolute joint local poses for bodies
+    # distanceJoint.CreateLocalPos0Attr().Set(Gf.Vec3f(0.0, 60.0, 0.0))
+    # distanceJoint.CreateLocalRot0Attr().Set(Gf.Quatf(1.0))
+    #
+    # distanceJoint.CreateLocalPos1Attr().Set(Gf.Vec3f(0.0, -60.0, 0.0))
+    # distanceJoint.CreateLocalRot1Attr().Set(Gf.Quatf(1.0))
+    #
+    # # set break force/torque
+    # distanceJoint.CreateBreakForceAttr().Set(1e20)
+    # distanceJoint.CreateBreakTorqueAttr().Set(1e20)
 
-    # define revolute joint axis and its limits, defined in degrees
-    distanceJoint.CreateMinDistanceAttr(-10)
-    distanceJoint.CreateMaxDistanceAttr(10.0)
-
-    # define revolute joint bodies
-    distanceJoint.CreateBody0Rel().SetTargets(["/box0"])
-    distanceJoint.CreateBody1Rel().SetTargets(["/box1"])
-
-    # define revolute joint local poses for bodies
-    distanceJoint.CreateLocalPos0Attr().Set(Gf.Vec3f(0.0, 60.0, 0.0))
-    distanceJoint.CreateLocalRot0Attr().Set(Gf.Quatf(1.0))
-
-    distanceJoint.CreateLocalPos1Attr().Set(Gf.Vec3f(0.0, -60.0, 0.0))
-    distanceJoint.CreateLocalRot1Attr().Set(Gf.Quatf(1.0))
-
-    # set break force/torque
-    distanceJoint.CreateBreakForceAttr().Set(1e20)
-    distanceJoint.CreateBreakTorqueAttr().Set(1e20)
-
+    #===================================================================================================================
     # optionally add angular drive for example
-    angularDriveAPI = UsdPhysics.DriveAPI.Apply(stage.GetPrimAtPath("/revoluteJoint"), "angular")
-    angularDriveAPI.CreateTypeAttr("force")
-    angularDriveAPI.CreateMaxForceAttr(1e20)
-    angularDriveAPI.CreateTargetVelocityAttr(1.0)
-    angularDriveAPI.CreateDampingAttr(1e10)
-    angularDriveAPI.CreateStiffnessAttr(0.0)
+    # angularDriveAPI = UsdPhysics.DriveAPI.Apply(stage.GetPrimAtPath("/revoluteJoint"), "angular")
+    # angularDriveAPI.CreateTypeAttr("force")
+    # angularDriveAPI.CreateMaxForceAttr(1e20)
+    # angularDriveAPI.CreateTargetVelocityAttr(1.0)
+    # angularDriveAPI.CreateDampingAttr(1e10)
+    # angularDriveAPI.CreateStiffnessAttr(0.0)
 
     stage.Save()
